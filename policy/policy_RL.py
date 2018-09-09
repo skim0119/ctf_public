8b67391492c8566cba7a3c50cd4b275844d7be42
Seung Hyun Kim <skim0119@gmail.com>
2018-09-08 20:45:20 -0500

    finished policy(network) testing module

--- policy/policy_RL.py
+++ policy/policy_RL.py
@@ -9,7 +9,7 @@ DOs/Denis Osipychev
 
 Last Modified:
     Seung Hyun Kim
-    Sat Sep  8 19:38:43 2018
+    created : Sat Sep  8 19:38:43 2018
 """
 
 import numpy as np
@@ -50,10 +50,38 @@ class PolicyGen:
             print('Graph is succesfully loaded.')
         else:
             print('Error : Graph is not loaded')
+            return
 
-    def __destructor(self):
-        pass
+        self.graph = tf.get_default_graph()
+        self.state = self.graph.get_tensor_by_name("state:0")
+        self.action = self.graph.get_tensor_by_name("action:0")
 
+    def policy(self, agent, obs, agent_id):
+        """ Policy
+        This method generate an action for given agent.
+        Agent is given with limited vision of a field.
+        Action is driven from pre-learned network.
+        Network will not update for an action.
+        """
+        # Expand the observation with 3-thickness wall
+        # - in order to avoid dealing with the boundary
+        obsx, obsy = obs.shape
+        _obs = np.ones((obsx+6, obsy+6)) * 8
+        _obs[3:3+obsx, 3:3+obsy] = obs
+        obs = _obs
+
+        # Initialize Variables
+        x, y = agent.get_loc()
+        x += 3
+        y += 3
+        view = obs[x-2:x+3, y-2:y+3] # limited view for the agent
+
+        # Run Graph
+        a_dist = self.sess(self.action, feed_dict={self.input:[view]})
+        a = np.random.choice(a_dist[0],p=a_dist[0])
+        a = np.argmax(a_dist == a)
+
+        return a
         
     def gen_action(self, agent_list, observation, free_map=None):
         """Action generation method.
@@ -68,11 +96,15 @@ class PolicyGen:
             
         Returns:
             action_out (list): list of integers as actions selected for team.
+
+        Note:
+            The graph is not updated in this session. It only returns action for given input.
         """
 
         action_out = []
 
-        for i in agent_list:
-            action_out.append(self.random.randint(0, 5)) # choose random action
+        for idx, agent in enumerate(agent_list):
+            action_out.append(self.policy(agent, observation, idx)
+            #action_out.append(self.random.randint(0, 5)) # choose random action
         
         return action_out
