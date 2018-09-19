83417ad1fad27a640ffebeb94fbeeb80a48bbb64
Seung Hyun Kim <skim0119@gmail.com>
2018-09-19 17:00:15 -0500

    initialize weight and model, increase vision size to 20

--- policy/policy_RL.py
+++ policy/policy_RL.py
@@ -101,8 +101,10 @@ class PolicyGen:
             The graph is not updated in this session. It only returns action for given input.
         """
 
+        print(observation)
         view = self.one_hot_encoder(observation, agent_list)
         ap = self.sess.run(self.action, feed_dict={self.state:view}) # Action Probability
+        print(ap)
         action_out = [np.random.choice(5, p=ap[x]/sum(ap[x])) for x in range(len(agent_list))]
 
         return action_out
