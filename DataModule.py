f6da0525d01e38591b089eb4926c3df41c7dc092
skim0119 <skim0119@gmail.com>
2018-10-05 11:22:51 +0900

    changed one-hot encoder to speed up

--- DataModule.py
+++ DataModule.py
@@ -17,8 +17,10 @@ VISION_RANGE = 10 #CONST.UGV_RANGE # Regardless, the vision will be 20x20
 VISION_dX    = 2*VISION_RANGE+1
 VISION_dY    = 2*VISION_RANGE+1
 
+#@profile
 def one_hot_encoder(state, agents):
     ret = np.zeros((len(agents),VISION_dX,VISION_dY,6))
+
     # team 1 : (1), team 2 : (-1), map elements: (0)
     map_channel = {UNKNOWN:0, DEAD:0,
                    TEAM1_BG:1, TEAM2_BG:1,
@@ -26,16 +28,17 @@ def one_hot_encoder(state, agents):
                    3:3, 5:3, # UAV, does not need to be included for now
                    TEAM1_FL:4, TEAM2_FL:4,
                    OBSTACLE:5}
-    map_color   = {UNKNOWN:1, DEAD:0, OBSTACLE:1,
-                   TEAM1_BG:1, TEAM2_BG:-1,
+    map_color   = {UNKNOWN:1, DEAD:0, 
+                   TEAM1_BG:0, TEAM2_BG:1,
                    TEAM1_AG:1, TEAM2_AG:-1,
                    3:1, 5:-1, # UAV, does not need to be included for now
-                   TEAM1_FL:1, TEAM2_FL:-1}
+                   TEAM1_FL:1, TEAM2_FL:-1,
+                   OBSTACLE:1}
     
     # Expand the observation with 3-thickness wall
     # - in order to avoid dealing with the boundary
     sx, sy = state.shape
-    _state = np.ones((sx+2*VISION_RANGE, sy+2*VISION_RANGE)) * OBSTACLE # 8 for obstacle
+    _state = np.full((sx+2*VISION_RANGE, sy+2*VISION_RANGE),OBSTACLE)
     _state[VISION_RANGE:VISION_RANGE+sx, VISION_RANGE:VISION_RANGE+sy] = state
     state = _state
 
@@ -44,10 +47,26 @@ def one_hot_encoder(state, agents):
         x, y = agent.get_loc()
         x += VISION_RANGE
         y += VISION_RANGE
-        vision = state[x-VISION_RANGE:x+VISION_RANGE+1,y-VISION_RANGE:y+VISION_RANGE+1] # extract the limited view for the agent (5x5)
-        for i in range(len(vision)):
-            for j in range(len(vision[0])):
-                if vision[i][j] != -1:
-                    channel = map_channel[vision[i][j]]
-                    ret[idx][i][j][channel] = map_color[vision[i][j]]
+        vision = state[x-VISION_RANGE:x+VISION_RANGE+1,y-VISION_RANGE:y+VISION_RANGE+1] # extract the limited view for the agent
+        
+        for channel, val in map_color.items():
+            if not val: # if val == 0, calculation is un-necessary
+                if val == 1:
+                    ret[idx,:,:,map_channel[channel]] += (vision == obj).astype(np.int32)
+                elif val == -1:
+                    ret[idx,:,:,map_channel[channel]] -= (vision == obj).astype(np.int32)
+        
     return ret
+
+if __name__ == '__main__':
+    import gym
+    # Tester
+    env = gym.make("cap-v0")
+    s = env.reset(map_size=20)
+    
+    print('start running')
+    for _ in range(1000):
+        s = env.reset(map_size=20)
+        if _ % 100 == 0 and _ != 0:
+            print(_)
+        one_hot_encoder(s, env.get_team_blue)
