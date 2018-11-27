cf8e447bdf5c6154069c5f9c2232504b184c8d3e
Seung Hyun Kim <skim0119@gmail.com>
2018-11-26 20:01:09 -0600

    add lstm script from awjuliani

--- network/ActorCritic.py
+++ network/ActorCritic.py
@@ -16,17 +16,17 @@ class ActorCritic():
                  decay_lr=False,
                  lr_actor=1e-4,
                  lr_critic=1e-4,
-                 grad_clip_norm = 0,
+                 grad_clip_norm=0,
                  global_step=None,
                  initial_step=0,
-                 trainable = False,
-                 lr_a_gamma = 1,
-                 lr_c_gamma = 1,
-                 lr_a_decay_step = 0,
-                 lr_c_decay_step = 0,
+                 lr_a_gamma=1,
+                 lr_c_gamma=1,
+                 lr_a_step=0,
+                 lr_c_step=0,
                  entropy_beta = 0.001,
                  sess=None,
-                 globalAC=None):
+                 global_network=None,
+                 lstm_network=True):
         """ Initialize AC network and required parameters
 
         Keyword arguments:
@@ -35,25 +35,27 @@ class ActorCritic():
         grad_clip_norm  - gradient clip
         scope           - name of the scope. (At least one 'global' network required)
         sess            - tensorflow session# Learning Rate Variables
-        globalAC        - use to update or pull network weights from global network
+        global_network        - use to update or pull network weights from global network
         """
 
+        # Class Environment
         self.sess=sess
 
-        # Parameters
+        # Parameters & Configs
         self.in_size = in_size
         self.action_size = action_size
         self.grad_clip_norm = grad_clip_norm
         self.scope = scope
         self.global_step = global_step
+        self.lstm_network=lstm_network
         
         with tf.variable_scope(scope):
             self.local_step = tf.Variable(initial_step, trainable=False, name='local_step')
             # Learning Rate Variables
             self.lr_actor = tf.train.exponential_decay(lr_actor, self.local_step,
-                                                       lr_a_decay_step, lr_a_gamma, staircase=True, name='lr_actor')
+                                                       lr_a_step, lr_a_gamma, staircase=True, name='lr_actor')
             self.lr_critic = tf.train.exponential_decay(lr_critic, self.local_step,
-                                                       lr_c_decay_step, lr_c_gamma, staircase=True, name='lr_critic')
+                                                       lr_c_step, lr_c_gamma, staircase=True, name='lr_critic')
 
             # Optimizer
             self.critic_optimizer = tf.train.AdamOptimizer(self.lr_critic, name='Adam_critic')
@@ -100,66 +102,87 @@ class ActorCritic():
                     with tf.name_scope('sync'):
                         # Pull global weights to local weights
                         with tf.name_scope('pull'):
-                            self.pull_a_vars_op = [local_var.assign(glob_var) for local_var, glob_var in zip(self.a_vars, globalAC.a_vars)]
-                            self.pull_c_vars_op = [local_var.assign(glob_var) for local_var, glob_var in zip(self.c_vars, globalAC.c_vars)]
+                            self.pull_a_vars_op = [local_var.assign(glob_var) for local_var, glob_var in zip(self.a_vars, global_network.a_vars)]
+                            self.pull_c_vars_op = [local_var.assign(glob_var) for local_var, glob_var in zip(self.c_vars, global_network.c_vars)]
 
                         # Push local weights to global weights
                         with tf.name_scope('push'):
-                            self.update_a_op = self.actor_optimizer.apply_gradients(zip(self.a_grads, globalAC.a_vars))
-                            self.update_c_op = self.critic_optimizer.apply_gradients(zip(self.c_grads, globalAC.c_vars))
+                            self.update_a_op = self.actor_optimizer.apply_gradients(zip(self.a_grads, global_network.a_vars))
+                            self.update_c_op = self.critic_optimizer.apply_gradients(zip(self.c_grads, global_network.c_vars))
                             
         '''for var in tf.trainable_variables(scope=self.scope):
             tf.summary.histogram(var.name, var)
         merged_summary_op = tf.summary.merge_all()'''
                             
     def build_network(self, init_mean=-1.0, init_std=1.0):
-        layer = layers.conv2d(self.state_input, 32, [5,5],
-                              activation_fn=tf.nn.relu,
-                              weights_initializer=layers.xavier_initializer_conv2d(),
-                              biases_initializer=tf.zeros_initializer(),
-                              padding='SAME')
-        layer = layers.max_pool2d(layer, [2,2])
-        layer = layers.conv2d(layer, 64, [3,3],
-                              activation_fn=tf.nn.relu,
-                              weights_initializer=layers.xavier_initializer_conv2d(),
-                              biases_initializer=tf.zeros_initializer(),
-                              padding='SAME')
-        layer = layers.max_pool2d(layer, [2,2])
-        layer = layers.conv2d(layer, 64, [2,2],
-                              activation_fn=tf.nn.relu,
-                              weights_initializer=layers.xavier_initializer_conv2d(),
-                              biases_initializer=tf.zeros_initializer(),
-                              padding='SAME')
-        layer = layers.flatten(layer)
-        
         with tf.variable_scope('actor'):
-            self.actor = layers.fully_connected(layer,
-                                                128,
-                                                weights_initializer=layers.xavier_initializer(),
-                                                biases_initializer=tf.zeros_initializer(),
-                                                activation_fn=tf.nn.relu)
-            self.actor = layers.fully_connected(self.actor,
+            net = self.state_input
+            '''net = layers.conv2d(net , 32, [5,5],
+                                activation_fn=tf.nn.elu,
+                                weights_initializer=layers.xavier_initializer_conv2d(),
+                                biases_initializer=tf.zeros_initializer(),
+                                padding='SAME')
+            net = layers.max_pool2d(net, [2,2])'''
+            net = layers.conv2d(net, 32, [3,3],
+                                activation_fn=tf.nn.elu,
+                                weights_initializer=layers.xavier_initializer_conv2d(),
+                                biases_initializer=tf.zeros_initializer(),
+                                padding='SAME')
+            net = layers.max_pool2d(net, [2,2])
+            net = layers.conv2d(net, 64, [2,2],
+                                activation_fn=tf.nn.elu,
+                                weights_initializer=layers.xavier_initializer_conv2d(),
+                                biases_initializer=tf.zeros_initializer(),
+                                padding='SAME')
+            net = layers.flatten(net)
+            net = layers.fully_connected(net,
+                                         128,
+                                         activation_fn=tf.nn.elu)
+
+            if self.lstm_network:
+                with tf.contrib.rnn as rnn:
+                    cell = rnn.BasicLSTMCell(128, state_is_tuple=True)
+                    cell_size, hidd_size = cell.state_size.c, cell.state_size.h
+                    cell_init = np.zeros((1, cell_size), np.float32)
+                    hidd_init = np.zeros((1, hidd_size), np.float32)
+                    cell_in = tf.placeholder(tf.float32, [1, cell_size])
+                    hidd_in = tf.placeholder(tf.float32, [1, hidd_size])
+                    
+                    self.state_init = [cell_init, hidd_init]
+                    self.state_in = [cell_in, hidd_in]
+                    rnn_in = tf.expand_dims(net,[0])
+                    step_size=tf.shape(self.state_input)[:1]
+                    state_in = rnn.LSTMStateTuple(cell_in, hidd_in)
+                    lstm_outputs, lstm_state = tf.nn.dynamic_rnn(cell, rnn_in,
+                                                                 initial_state   = state_in,
+                                                                 sequence_length = step_size,
+                                                                 time_major      = False)
+                    lstm_cell, lstm_hidd = lstm_state
+                    self.state_out = (lstm_cell[:1, :], lstm_hidd[:1, :])
+                    net = tf.reshape(lstm_output, [-1,128])
+        
+            self.actor = layers.fully_connected(net
                                                 self.action_size,
                                                 weights_initializer=layers.xavier_initializer(),
                                                 biases_initializer=tf.zeros_initializer(),
                                                 activation_fn=tf.nn.softmax)
 
         with tf.variable_scope('critic'):
-            self.critic = layers.fully_connected(layer, 1,
+            self.critic = layers.fully_connected(net, 1,
                                                  weights_initializer=layers.xavier_initializer(),
                                                  biases_initializer=tf.zeros_initializer(),
                                                  activation_fn=None)
             self.critic = tf.reshape(self.critic, [-1])
 
-        common_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self.scope + '/Conv')
-        a_vars = common_vars+tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self.scope + '/actor')
+        a_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self.scope + '/actor')
         c_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self.scope + '/critic')
 
         return a_vars, c_vars
 
     # Update global network with local gradients
     def update_global(self, feed_dict):
-        al, cl, etrpy, _, __ = self.sess.run([self.actor_loss, self.critic_loss, self.entropy, self.update_a_op, self.update_c_op], feed_dict)
+        self.sess.run([self.update_a_op, self.update_c_op], feed_dict)
+        al, cl, etrpy = self.sess.run([self.actor_loss, self.critic_loss, self.entropy], feed_dict)
         
         return al, cl, etrpy
 
