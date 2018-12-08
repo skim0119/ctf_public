8ed0b677d213f80b64860c753b22f6c077b1ecc5
Seung Hyun Kim <skim0119@gmail.com>
2018-12-08 08:04:48 -0600

    add 11 channel OH binary representation

--- utility/dataModule.py
+++ utility/dataModule.py
@@ -1,87 +1,159 @@
 import numpy as np
-import tensorflow as tf
 import gym_cap.envs.const as CONST
 
-UNKNOWN  = CONST.UNKNOWN # -1
+UNKNOWN  = CONST.UNKNOWN          # -1
 TEAM1_BG = CONST.TEAM1_BACKGROUND # 0
 TEAM2_BG = CONST.TEAM2_BACKGROUND # 1
-TEAM1_AG = CONST.TEAM1_UGV # 2
-TEAM2_AG = CONST.TEAM2_UGV # 4
-TEAM1_FL = CONST.TEAM1_FLAG # 6
-TEAM2_FL = CONST.TEAM2_FLAG # 7
-OBSTACLE = CONST.OBSTACLE # 8
-DEAD     = CONST.DEAD # 9
-SELECTED = CONST.SELECTED # 10
-COMPLETED= CONST.COMPLETED # 11
+TEAM1_AG = CONST.TEAM1_UGV        # 2
+TEAM1_UAV= CONST.TEAM1_UAV        # 3
+TEAM2_AG = CONST.TEAM2_UGV        # 4
+TEAM2_UAV= CONST.TEAM2_UAV        # 5
+TEAM1_FL = CONST.TEAM1_FLAG       # 6
+TEAM2_FL = CONST.TEAM2_FLAG       # 7
+OBSTACLE = CONST.OBSTACLE         # 8
+DEAD     = CONST.DEAD             # 9
+SELECTED = CONST.SELECTED         # 10
+COMPLETED= CONST.COMPLETED        # 11
 
-def one_hot_encoder(state, agents, VISION_RANGE=9, reverse=False):
+def one_hot_encoder(state, agents, vision_radius=9, reverse=False):
     """Encoding pipeline for CtF state to one-hot representation
 
-    Keyword arguments:
-    state -- CtF state in raw format
-    VISION_RANGE -- Size of the vision range (default=9)`
-    reverse -- Reverse the color. Used for red-perspective (default=False)
+    6-channel one-hot representation of state.
+    State is not binary: team2 is represented with -1.
+    Channels are not symmetrical.
+
+    :param state: CtF state in raw format
+    :param agents: Agent list of CtF environment
+    :param vision_radius: Size of the vision range (default=9)`
+    :param reverse:Reverse the color. Used for red-perspective (default=False)
+
+    :return oh_state: One-hot encoded state
     """
-    VISION_dX    = 2*VISION_RANGE+1
-    VISION_dY    = 2*VISION_RANGE+1
-    ret = np.zeros((len(agents),VISION_dX,VISION_dY,6))
+
+    vision_lx = 2*vision_radius+1
+    vision_ly = 2*vision_radius+1
+    oh_state = np.zeros((len(agents),vision_lx,vision_ly,6))
 
     # team 1 : (1), team 2 : (-1), map elements: (0)
     map_channel = {UNKNOWN:0, DEAD:0,
                    TEAM1_BG:1, TEAM2_BG:1,
                    TEAM1_AG:2, TEAM2_AG:2,
-                   3:3, 5:3, # UAV, does not need to be included for now
+                   TEAM1_UAV:3, TEAM2_UAV:3,
                    TEAM1_FL:4, TEAM2_FL:4,
                    OBSTACLE:5}
     if not reverse:
-        map_color   = {UNKNOWN:1, DEAD:0, 
-                       TEAM1_BG:0, TEAM2_BG:1,
-                       TEAM1_AG:1, TEAM2_AG:-1,
-                       3:1, 5:-1, # UAV, does not need to be included for now
-                       TEAM1_FL:1, TEAM2_FL:-1,
-                       OBSTACLE:1}
+        map_color = {UNKNOWN:1, DEAD:0, 
+                     TEAM1_BG:0, TEAM2_BG:1,
+                     TEAM1_AG:1, TEAM2_AG:-1,
+                     TEAM1_UAV:1, TEAM2_UAV:-1,
+                     TEAM1_FL:1, TEAM2_FL:-1,
+                     OBSTACLE:1}
     else: # reverse color
-        map_color   = {UNKNOWN:1, DEAD:0, 
-                       TEAM1_BG:1, TEAM2_BG:0,
-                       TEAM1_AG:-1, TEAM2_AG:1,
-                       3:-1, 5:1, # UAV, does not need to be included for now
-                       TEAM1_FL:-1, TEAM2_FL:1,
-                       OBSTACLE:1}
+        map_color = {UNKNOWN:1, DEAD:0, 
+                     TEAM1_BG:1, TEAM2_BG:0,
+                     TEAM1_AG:-1, TEAM2_AG:1,
+                     TEAM1_UAV:-1, TEAM2_UAV:1,
+                     TEAM1_FL:-1, TEAM2_FL:1,
+                     OBSTACLE:1}
         
 
-    # Expand the observation with 3-thickness wall
-    # - in order to avoid dealing with the boundary
+    # Expand the observation with wall to avoid dealing with the boundary
     sx, sy = state.shape
-    _state = np.full((sx+2*VISION_RANGE, sy+2*VISION_RANGE),OBSTACLE)
-    _state[VISION_RANGE:VISION_RANGE+sx, VISION_RANGE:VISION_RANGE+sy] = state
+    _state = np.full((sx+2*vision_radius, sy+2*vision_radius),OBSTACLE)
+    _state[vision_radius:vision_radius+sx, vision_radius:vision_radius+sy] = state
     state = _state
 
     for idx,agent in enumerate(agents):
         # Initialize Variables
         x, y = agent.get_loc()
-        x += VISION_RANGE
-        y += VISION_RANGE
-        vision = state[x-VISION_RANGE:x+VISION_RANGE+1,y-VISION_RANGE:y+VISION_RANGE+1] # extract the limited view for the agent
+        x += vision_radius
+        y += vision_radius
+        vision = state[x-vision_radius:x+vision_radius+1,y-vision_radius:y+vision_radius+1] # extract view
         
         # FULL MATRIX OPERATION
         for channel, val in map_color.items():
             if val == 1:
-                ret[idx,:,:,map_channel[channel]] += (vision == channel).astype(np.int32)
+                oh_state[idx,:,:,map_channel[channel]] += (vision == channel).astype(np.int32)
             elif val == -1:
-                ret[idx,:,:,map_channel[channel]] -= (vision == channel).astype(np.int32)
+                oh_state[idx,:,:,map_channel[channel]] -= (vision == channel).astype(np.int32)
                 
-    return ret
+    return oh_state
 
-if __name__ == '__main__':
+def one_hot_encoder_v2(state, agents, vision_radius=9, reverse=False):
+    """ Encoding pipeline for CtF state to one-hot representation
+    11-channel one-hot representation of state.
+    State is binary.
+    Some optimization is included.
+
+    :param state: CtF state in raw format
+    :param agents: Agent list of CtF environment
+    :param vision_radius: Size of the vision range (default=9)`
+    :param reverse:Reverse the color. Used for red-perspective (default=False)
+
+    :return oh_state: One-hot encoded state
+    """
+
+    num_channel = 11
+    num_agents = len(agents)
+
+    vision_lx = 2*vision_radius+1
+    vision_ly = 2*vision_radius+1
+
+    # Map channel for each elements
+    if not reverse:
+        order = [UNKNOWN, OBSTACLE, TEAM1_BG, TEAM2_BG, TEAM1_AG, TEAM2_AG,
+                                  TEAM1_UAV, TEAM2_UAV, TEAM1_FL, TEAM2_FL, DEAD]
+    else: # reverse mapping
+        order = [UNKNOWN, OBSTACLE, TEAM2_BG, TEAM1_BG, TEAM2_AG, TEAM1_AG,
+                                  TEAM2_UAV, TEAM1_UAV, TEAM2_FL, TEAM1_FL, DEAD]
+    map_channel = dict(zip(order, range(num_channel)))
+
+    # Expand the observation with wall to avoid dealing with the boundary
+    sx, sy = state.shape
+    _state = np.full((sx+2*vision_radius, sy+2*vision_radius),OBSTACLE)
+    _state[vision_radius:vision_radius+sx, vision_radius:vision_radius+sy] = state
+    state = _state
+
+    each_agent = []
+    for idx, agent in enumerate(agents):
+        # Initialize Variables
+        x, y = agent.get_loc()
+        x += vision_radius
+        y += vision_radius
+        vision = state[x-vision_radius:x+vision_radius+1,y-vision_radius:y+vision_radius+1] # extract view
+        
+        # operation
+        each_channel = []
+        for element, channel in map_channel.items():
+            each_channel.append(vision==element)
+        each_agent.append(np.stack(each_channel, axis=-1))
+    oh_state = np.stack(each_agent, axis=0)
+    print(oh_state.shape)
+                
+    return oh_state
+
+# Debug
+def debug():
     import gym
-    # Tester
+    import time
     env = gym.make("cap-v0")
     s = env.reset(map_size=20)
     
     print('start running')
-    for _ in range(1000):
+    stime = time.time()
+    for _ in range(3000):
         s = env.reset(map_size=20)
-        if _ % 100 == 0 and _ != 0:
-            print(_)
         one_hot_encoder(s, env.get_team_blue)
-    print('finish testing for one-hot-encoder')
+    print(f'Finish testing for one-hot-encoder: {time.time()-stime} sec')
+
+    s = env.reset(map_size=20)
+    
+    print('start running v2')
+    stime = time.time()
+    for _ in range(3000):
+        s = env.reset(map_size=20)
+        one_hot_encoder_v2(s, env.get_team_blue)
+    print(f'Finish testing for one-hot-encoder: {time.time()-stime} sec')
+
+if __name__ == '__main__':
+    debug()
