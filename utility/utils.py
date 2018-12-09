e466ea9a68b8587cc6253a5c192f4ac811d457d8
Seung Hyun Kim <skim0119@gmail.com>
2018-12-09 13:26:23 -0600

    add comments

--- utility/utils.py
+++ utility/utils.py
@@ -1,4 +1,3 @@
-from collections import deque 
 import numpy as np
 import random
 
@@ -49,16 +48,16 @@ def discount_rewards(rewards, gamma, normalize=False):
     
     """
 
-    discReward = np.zeros_like(rewards)
-    runningAdd = 0.0
+    disc_reward = np.zeros_like(rewards)
+    cumulate_add = 0.0
     for idx, reward in enumerate(reversed(rewards)):
-        runningAdd = (runningAdd * gamma + reward)
-        discReward[idx] = runningAdd
+        cumulate_add = (cumulate_add * gamma + reward)
+        disc_reward[idx] = cumulate_add
 
     if normalize:
-        discReward = (discReward - np.mean(discReward)) / (np.std(discReward)+1e-8) # normalize
+        disc_reward = (disc_reward - np.mean(disc_reward)) / (np.std(disc_reward)+1e-8) # normalize
 
-    return discReward
+    return disc_reward
 
 def normalize(r):
     """ take 1D float numpy array and normalize it
@@ -111,33 +110,89 @@ def retrace_prod(targets, behaviors, lambda_ = 0.2):
     return np.cumprod(retrace(targets, behaviors, lambda_))
 
 class MovingAverage:
+    """MovingAverage
+
+    Container that only store give size of element, and store moving average.
+    Queue structure of container.
+
+    Methods:
+        __init__
+        __call__
+        tolist 
+        extend (list)
+        append (int)
+        clear
+
+    """
     def __init__(self, size):
-        self.ma = 0.0
+        """__init__
+
+        :param size: number of element that will be stored in he container
+        """
+        from collections import deque 
+        self.average = 0.0
         self.size = size
         
         self.queue = deque(maxlen=size)
         
     def __call__(self):
-        return self.ma
+        """__call__"""
+        return self.average
     
     def tolist(self):
+        """tolist
+        Return the elements in the container in (list) structure
+        """
         return list(self.queue)
 
     def extend(self, l:list):
+        """extend
+        
+        Similar to list.extend
+
+        :param l (list): list of number that will be extended in the deque
+        """
         # Append list of numbers
         self.queue.extend(l)
         self.size = len(self.queue)
-        self.ma = sum(self.queue) / self.size
+        self.average = sum(self.queue) / self.size
         
     def append(self, n):
+        """append
+
+        Element-wise appending in the container
+
+        :param n: number that will be appended on the container.
+        """
         s = len(self.queue)
         if s == self.size:
-            self.ma = ((self.ma * self.size) - self.queue[0] + n) / self.size
+            self.average = ((self.average * self.size) - self.queue[0] + n) / self.size
         else:
-            self.ma = (self.ma * s + n) / (s + 1)
+            self.average = (self.average * s + n) / (s + 1)
         self.queue.append(n)
 
-class Experience_buffer():
+    def clear(self):
+        """clear
+        reset the container
+        """
+        self.average = 0.0
+        self.queue.clear()
+
+class Experience_buffer:
+    """Experience_buffer
+    Experience buffer use for storing tuples for MDP.
+    Support returning and shuffling features
+
+    Method:
+        __init__ (int, int)
+        __len__
+        add (list)
+        add_element (object)
+        flush
+        empty
+        sample (int, bool)
+        pop (int, bool)
+    """
     def __init__(self, experience_shape=4, buffer_size = 50000):
         self.buffer = []
         self.buffer_size = buffer_size
@@ -146,7 +201,7 @@ class Experience_buffer():
     def __len__(self):
         return len(self.buffer)
     
-    def add(self,experience):
+    def add(self, experience):
         if len(self.buffer) + len(experience) >= self.buffer_size:
             self.buffer[0:(len(experience)+len(self.buffer))-self.buffer_size] = []
         self.buffer.extend(experience)
