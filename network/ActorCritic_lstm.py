05eb930d9627b0c8f0142601adc158b4cdca3883
Seung Hyun Kim <skim0119@gmail.com>
2018-12-16 02:11:04 -0600

    fix bug

--- network/ActorCritic_lstm.py
+++ network/ActorCritic_lstm.py
@@ -84,9 +84,6 @@ class ActorCritic:
         self.separate_train = separate_train
         self.asynch_training=asynch_training
         
-        # Dimensions
-        self.lstm_layers = 1
-        
         with tf.variable_scope(scope):
             self.local_step = tf.Variable(initial_step, trainable=False, name='local_step')
             # Learning Rate Variables
@@ -190,74 +187,72 @@ class ActorCritic:
     def _build_network(self):
         with tf.variable_scope('actor'):
             net = self.state_input
-            net = layers.conv2d(net , 32, [5,5],
+            net = layers.conv2d(net , 32, [3,3],
                                 activation_fn=tf.nn.relu,
                                 weights_initializer=layers.xavier_initializer_conv2d(),
                                 biases_initializer=tf.zeros_initializer(),
                                 padding='SAME')
-            net = layers.max_pool2d(net, [2,2])
-            net = layers.conv2d(net, 64, [3,3],
-                                activation_fn=tf.nn.relu,
-                                weights_initializer=layers.xavier_initializer_conv2d(),
-                                biases_initializer=tf.zeros_initializer(),
-                                padding='SAME')
-            net = layers.max_pool2d(net, [2,2])
             net = layers.conv2d(net, 64, [2,2],
                                 activation_fn=tf.nn.relu,
                                 weights_initializer=layers.xavier_initializer_conv2d(),
                                 biases_initializer=tf.zeros_initializer(),
                                 padding='SAME')
             net = layers.flatten(net)
-            net = layers.fully_connected(net, 128)
+            net = layers.fully_connected(net, 256)
 
             if self.lstm_network:
-                '''rnn_size = [2, 1, 19, 19, 32]
-                self.rnn_state_ = tf.placeholder(tf.float32, rnn_size)
-                self.rnn_init_state = np.zeros(rnn_size)
+                rnn_steps = 256
+                rnn_layers = 1
+                if False: # self.lstm_GRU:
+                    self.rnn_state_ = tf.placeholder(tf.float32, [self.lstm_layers, 1, rnn_steps])
+                    self.rnn_init_state = np.zeros((self.lstm_layers, 1, rnn_steps))
+                    state_per_layer_list = tf.unstack(self.rnn_state_, axis=0)
+                    rnn_tuple_state = tuple([holder_ for holder_ in state_per_layer_list])
+                    cell = tf.nn.rnn_cell.GRUCell(rnn_steps, name='gru_cell')
+                    cell = tf.nn.rnn_cell.MultiRNNCell([cell] * self.lstm_layers)
+                    states_series, self.current_state = tf.nn.dynamic_rnn(cell,
+                                                                          tf.expand_dims(net, [0]),
+                                                                          initial_state=rnn_tuple_state,
+                                                                          sequence_length=tf.shape(self.state_input)[:1]
+                                                                          )
+                    net = tf.reshape(states_series[-1], [-1, rnn_steps])
+                else:
+                    # Using basic lstm
+                    #Recurrent network for temporal dependencies (awjuliani) for comparison
+                    #lstm_cell = tf.contrib.rnn.BasicLSTMCell(256)
+                    #c_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.c])
+                    #h_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.h])
+                    #c_init = np.zeros((1, lstm_cell.state_size.c), np.float32)
+                    #h_init = np.zeros((1, lstm_cell.state_size.h), np.float32)
+                    #self.state_init = [c_init, h_init]
+                    #self.state_in = (c_in, h_in)
+                    #rnn_in = tf.expand_dims(hidden, [0])
+                    #step_size = tf.shape(self.imageIn)[:1]
+                    #state_in = tf.contrib.rnn.LSTMStateTuple(c_in, h_in)
+                    #lstm_outputs, lstm_state = tf.nn.dynamic_rnn(
+                    #                                        lstm_cell,
+                    #                                        rnn_in,
+                    #                                        initial_state=state_in,
+                    #                                        sequence_length=step_size,
+                    #                                        time_major=False
+                    #                                        )
+                    #lstm_c, lstm_h = lstm_state
+                    #self.state_out = (lstm_c[:1, :], lstm_h[:1, :])
+                    #rnn_out = tf.reshape(lstm_outputs, [-1, 256])
+#------------------------------------------------------------------------------------------------------------------------
+                    lstm_cell = tf.nn.rnn_cell.LSTMCell(rnn_steps)
+                    self.rnn_state_ = tf.placeholder(tf.float32, [rnn_layers, 2, 1, rnn_steps])
+                    self.rnn_state_init = np.zeros((rnn_layers, 2, 1, rnn_steps), np.float32)
+                    rnn_state_input = tf.contrib.rnn.LSTMStateTuple(*tf.unstack(self.rnn_state_,axis=0))
+                    #lstm_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * rnn_layers)
+                    sequence_length = tf.shape(self.imageIn)[0] #[:1]
+                    rnn_output, self.rnn_state = tf.nn.dynamic_rnn(cell=lstm_cell,
+                                                                   inputs=net,
+                                                                   sequence_length=sequence_length,
+                                                                   initial_state=rnn_state_in
+                                                                   )
+                    net = tf.reshape(rnn_output, [-1, rnn_steps])
 
-                state_per_layer_list = tf.split(net, 6, 3) # creates a list of leghth time_steps and one elemnt has the shape of (?, 400, 400, 1, 10)
-                state_per_layer_list = [net_ for net_ in state_per_layer_list] #remove the third dimention now one list elemnt has the shape of (?, 400, 400, 10)
-
-                cell = tf.contrib.rnn.ConvLSTMCell(conv_ndims=2, # ConvLSTMCell definition
-                                                   input_shape=[19, 19, 6],
-                                                   output_channels=32,
-                                                   kernel_shape=[2, 2],
-                                                   skip_connection=False)
-
-                state = cell.zero_state(1, dtype=tf.float32) #initial state is zero
-
-                with tf.variable_scope("ConvLSTM") as scope:  # as BasicLSTMCell # create the RNN with a loop
-                    for i, net_ in enumerate(state_per_layer_list):
-                        if i > 0:
-                            scope.reuse_variables()
-                        # ConvCell takes Tensor with size [1, 19, 19, 32].
-                        states_series, self.current_state = cell(net_,state)
-                net = layers.flatten(states_series[-1])'''
-
-                rnn_steps = 16
-                self.rnn_state_ = tf.placeholder(tf.float32, [self.lstm_layers, 1, rnn_steps])
-                self.rnn_init_state = np.zeros((self.lstm_layers, 1, rnn_steps))
-                #self.rnn_state_ = tf.placeholder(tf.float32, [self.lstm_layers, 2, 1, rnn_steps])
-                #self.rnn_init_state = np.zeros((self.lstm_layers, 2, 1, rnn_steps))
-
-                state_per_layer_list = tf.unstack(self.rnn_state_, axis=0)
-                rnn_tuple_state = tuple([holder_ for holder_ in state_per_layer_list])
-                #rnn_tuple_state = tuple(
-                #    [tf.nn.rnn_cell.LSTMStateTuple(state_per_layer_list[idx][0], state_per_layer_list[idx][1])
-                #     for idx in range(self.lstm_layers)]
-                #)
-
-                cell = tf.nn.rnn_cell.GRUCell(rnn_steps, name='gru_cell')
-                #cell = tf.nn.rnn_cell.LSTMCell(rnn_steps, forget_bias=1, name='lstm_cell')
-                cell = tf.nn.rnn_cell.MultiRNNCell([cell] * self.lstm_layers)
-                states_series, self.current_state = tf.nn.dynamic_rnn(cell,
-                                                                      tf.expand_dims(net, [0]),
-                                                                      initial_state=rnn_tuple_state,
-                                                                      sequence_length=tf.shape(self.state_input)[:1]
-                                                                      )
-                net = tf.reshape(states_series[-1], [-1, rnn_steps])
-                
-                
             self.actor = layers.fully_connected(net,
                                                 self.action_size,
                                                 weights_initializer=layers.xavier_initializer(),
@@ -273,8 +268,7 @@ class ActorCritic:
             self.critic = tf.reshape(self.critic, [-1])
 
     # Update global network with local gradients
-
-    # Choose Action
+    # Forward Propagation
     def run_network(self, feed_dict):
         if self.lstm_network:
             a_probs, critic, rnn_state = self.sess.run([self.actor, self.critic, self.current_state], feed_dict)
