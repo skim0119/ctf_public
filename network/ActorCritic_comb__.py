8fb435900356c557ee2c44df9f00bd8e8cdbe3da
Seung Hyun Kim <skim0119@gmail.com>
2018-12-20 16:06:01 -0600

    reorganize

--- network/ActorCritic_comb__.py
+++ network/ActorCritic_comb__.py
@@ -10,6 +10,28 @@ from network.base import base
 import utility
 
 class ActorCritic:
+    """ Build the graph for A3C model 
+
+    """
+    
+    def __init__(self, params):
+        raise NotImplementedError
+    def _create_placeholders(self):
+        """ Step 1: define the placeholders for input and output """
+        raise NotImplementedError
+    def _create_embedding(self):
+        """ Step 2: define weights. In word2vec, it's actually the weights that we care
+        about """
+        raise NotImplementedError
+    def _create_loss(self):
+        """ Step 3 + 4: define the inference + the loss function """
+        raise NotImplementedError
+    def _create_optimizer(self):
+        """ Step 5: define optimizer """
+        raise NotImplementedError
+
+
+class ActorCritic:
     """Actor Critic Network Implementation for A3C (Tensorflow)
 
     This module contains building network and pipelines to sync with global network.
