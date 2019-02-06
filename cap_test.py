61dd5c58383ef9dd1c2eeb349edace33e5c785b3
Seung Hyun Kim <skim0119@gmail.com>
2019-02-06 14:39:10 -0600

    enabled cap_test for A3C

--- cap_test.py
+++ cap_test.py
@@ -4,54 +4,43 @@ import gym_cap
 import numpy as np
 
 # the modules that you can use to generate the policy.
-import policy.patrol 
+import policy.patrol
 import policy.random
 import policy.roomba
-import policy.policy_RL
+import policy.policy_A3C
 import policy.zeros
 
-start_time = time.time()
-env = gym.make("cap-v0") # initialize the environment
 
-done = False
-t = 0
-total_score = 0
+start_time = time.time()
+env = gym.make("cap-v0").unwrapped  # initialize the environment
 
 # reset the environment and select the policies for each of the team
-policy_red  = policy.zeros.PolicyGen(env.get_map, env.get_team_red)
-#policy_red = policy.policy_RL.PolicyGen(env.get_map, env.get_team_red,
+policy_red = policy.roomba.PolicyGen(env.get_map, env.get_team_red)
+# policy_red = policy.policy_RL.PolicyGen(env.get_map, env.get_team_red,
 #                                         model_dir='model_pretrain/A3C_CVT/',
 #                                         input_name='global/actor/state:0',
 #                                         output_name='global/actor/fully_connected/Softmax:0',
 #                                         color='red'
 #                                         )
-policy_blue = policy.policy_RL.PolicyGen(env.get_map, env.get_team_blue,
-                                         model_dir='model_pretrain/A3C_CVT/',
-                                         input_name='global/actor/state:0',
-                                         output_name='global/actor/fully_connected/Softmax:0'
-                                         )
+policy_blue = policy.policy_A3C.PolicyGen(env.get_map, env.get_team_blue,
+                                          model_dir='model/A3C_CTF_Roomba/',
+                                          input_name='global/state:0',
+                                          output_name='global/actor/fully_connected_1/Softmax:0'
+                                          )
 observation = env.reset(map_size=20,
                         policy_blue=policy_blue,
                         policy_red=policy_red)
 
-pre_score = 0;
+pre_score = 0
+done = False
+t = 0
+total_score = 0
 while True:
-    t=0
+    t = 0
     while not done:
-        #you are free to select a random action
-        # or generate an action using the policy
-        # or select an action manually
-        # and the apply the selected action to blue team
-        # or use the policy selected and provided in env.reset 
-        #action = env.action_space.sample()  # choose random action
-        #action = [0, 0, 0, 0]
-        action = policy_blue.gen_action(env.get_team_blue, env._env)
+        action = policy_blue.gen_action(env.get_team_blue, env._env)  # Full observability
         observation, reward, done, info = env.step(action)
-        
-        #observation, reward, done, info = env.step()  # feedback from environment
-        #print(reward-pre_score, ' ',done)
-        #pre_score = reward;
-        
+
         # render and sleep are not needed for score analysis
         t += 1
         env.render(mode="fast")
@@ -61,4 +50,4 @@ while True:
     env.reset()
     done = False
     print("Time: %.2f s, score: %.2f" %
-        ((time.time() - start_time),reward))
+          ((time.time() - start_time), reward))
