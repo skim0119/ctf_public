a98af7e5fb1093f29812ebf20e64e912a8b401dc
Seung Hyun Kim <skim0119@gmail.com>
2019-02-25 02:57:25 -0600

    add deep layers conv2d and fc;

--- network/base.py
+++ network/base.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tensorflow.contrib.layers as layers
 import numpy as np
 
 
@@ -9,24 +10,38 @@ Notes:
 """
 
 
-class Base:
-    def __init__(self):
-        pass
+class Deep_layer:
+    @staticmethod
+    def conv2d_pool(input_layer, channels, kernels, pools,
+                    activation=tf.nn.relu, padding='SAME', flatten=False, reuse=False):
+        assert len(channels) == len(kernels)
+        net = input_layer
+        for idx, (ch, kern, pool) in enumerate(zip(channels, kernels, pools)):
+            net = layers.conv2d(net, ch, kern,
+                                activate_fn=activation,
+                                padding=padding,
+                                weights_initializer=layers.xavier_initializer_conv2d(),
+                                biases_initializer=tf.zeros_initializer(),
+                                reuse=reuse)
+            if pools[idx] > 1:
+                net = layers.max_pool2d(net, pool)
+        if flatten:
+            net = layers.flatten(net)
+        return net
 
-    def _build_placeholders(self):
-        raise NotImplementedError
-
-    def _build_embedding(self):
-        raise NotImplementedError
-
-    def _build_loss(self):
-        raise NotImplementedError
-
-    def _build_optimizer(self):
-        raise NotImplementedError
+    @staticmethod
+    def fc(input_layer, hidden_layers,
+           activation=tf.nn.relu, reuse=False):
+        net = input_layer
+        for idx, node in enumerate(hidden_layers):
+            net = layers.fully_connected(net, node
+                                         activate_fn=activation,
+                                         reuse=reuse)
+        return net
 
 
 class Custom_initializers:
+    @staticmethod
     def normalized_columns_initializer(std=1.0):
         def _initializer(shape, dtype=None, partition_info=None):
             out = np.random.randn(*shape).astype(np.float32)
@@ -34,6 +49,7 @@ class Custom_initializers:
             return tf.constant(out)
         return _initializer
 
+    @staticmethod
     def log_uniform_initializer(mu, std):
         def _initializer(shape, dtype=None, partition_info=None):
             out = np.random.lognormal(mean=mu, sigma=std, size=shape).astype(np.float32)
