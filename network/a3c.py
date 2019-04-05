42ed883a13335f2511a5c5fbf15a88c47ea00cf3
skim0119@gmail.com <skim0119@gmail.com>
2019-04-05 16:09:33 +0900

    pep8

--- network/a3c.py
+++ network/a3c.py
@@ -4,6 +4,9 @@ a3c.py module includes basic modules and implementation of A3C for CtF environme
 Some of the methods are left unimplemented which makes the a3c module to be parent abstraction.
 
 Script contains example A3C
+
+TODO:
+    - Include gradient and weight histograph for nan debug
 """
 
 import tensorflow as tf
@@ -17,6 +20,7 @@ from network.base import Deep_layer
 from network.pg import Loss, Backpropagation
 
 from network.base import Tensorboard_utility as TB
+
 
 class a3c:
     """ A3C Module
@@ -39,10 +43,10 @@ class a3c:
 
         trainable = global_network is not None
 
-        #$$$$$$$ Loss/Backpropagation $$$$$$$#
+        # $$$$$$$ Loss/Backpropagation $$$$$$$ #
         loss = kwargs.get('loss', Loss.softmax_cross_entropy_selection)
         backprop = kwargs.get('back_prop', Backpropagation.asynch_pipeline)
-        #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+        # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ #
 
         with tf.variable_scope(scope):
             self._build_placeholder(in_size, trainable)
@@ -63,22 +67,22 @@ class a3c:
                              lr_actor, lr_critic)
                 self.pull_op, self.update_ops = _
 
-        self.merged_summary_op = self._build_summary(self.a_vars+self.c_vars)
+        self.merged_summary_op = self._build_summary(self.a_vars + self.c_vars)
 
-    def _build_placeholder(self, input_shape, trainable:bool):
+    def _build_placeholder(self, input_shape, trainable: bool):
         self.state_input = tf.placeholder(shape=input_shape, dtype=tf.float32, name='state')
         if trainable:
             self.action_ = tf.placeholder(shape=[None], dtype=tf.int32, name='action_hold')
             self.td_target_ = tf.placeholder(shape=[None], dtype=tf.float32, name='td_target_hold')
             self.advantage_ = tf.placeholder(shape=[None], dtype=tf.float32, name='adv_hold')
 
-    def _build_summary(self, vars_list:list):
+    def _build_summary(self, vars_list: list):
         """ _build_summary (Implementation required)
 
         Given the list of tensor variables, returns summary operator of histogram.
         Mostly used for summarizing the weights of the layer.
 
-        ex) 
+        ex)
             var_record = _build_summary(actor_vars)
             sess.run(var_record, feed_dict)
 
@@ -93,7 +97,7 @@ class a3c:
 
         summaries = []
         for var in vars_list:
-            var_name = var.name.replace(":","_") # colon (:) is not allowed in TF board
+            var_name = var.name.replace(":", "_")  # colon (:) is not allowed in TF board
             summaries.append(tf.summary.histogram(var_name, var))
         return tf.summary.merge(summaries)
 
@@ -104,7 +108,7 @@ class a3c:
 
         Parameters
         ----------------
-        input_holder : [tf.placeholder] 
+        input_holder : [tf.placeholder]
 
         Returns
         ----------------
@@ -122,7 +126,7 @@ class a3c:
         """ run_network
         Parameters
         ----------------
-        states : [List/np.array] 
+        states : [List/np.array]
 
         Returns
         ----------------
@@ -130,12 +134,12 @@ class a3c:
             critic : [List]
         """
 
-        feed_dict = {self.state_input : states}
+        feed_dict = {self.state_input: states}
         a_probs, critic = self.sess.run([self.actor, self.critic], feed_dict)
-        return [np.random.choice(self.action_size, p=prob/sum(prob)) for prob in a_probs], critic
-    
+        return [np.random.choice(self.action_size, p=prob / sum(prob)) for prob in a_probs], critic
+
     def run_sample(self, states):
-        feed_dict = {self.state_input : states}
+        feed_dict = {self.state_input: states}
         a_probs, critic = self.sess.run([self.actor, self.critic], feed_dict)
         a_probs = self.sess.run(self.actor, feed_dict)
         return a_probs
@@ -147,8 +151,8 @@ class a3c:
 
         Parameters
         ----------------
-        log : [bool] 
-             logging option 
+        log : [bool]
+             logging option
 
         Returns
         ----------------
@@ -156,10 +160,10 @@ class a3c:
         closs : [Double]
         entropy : [Double]
         """
-        feed_dict = {self.state_input : state_input,
-                     self.action_ : action,
-                     self.td_target_ : td_target,
-                     self.advantage_ : advantage}
+        feed_dict = {self.state_input: state_input,
+                     self.action_: action,
+                     self.td_target_: td_target,
+                     self.advantage_: advantage}
         self.sess.run(self.update_ops, feed_dict)
 
         ops = [self.actor_loss, self.critic_loss, self.entropy]
@@ -190,18 +194,20 @@ class ActorCritic(a3c):
                  entropy_beta=0.001,
                  sess=None, global_network=None):
         """ Initialize AC network and required parameters """
-        super(ActorCritic, self).__init__(in_size, action_size,
-                scope, lr_actor, lr_critic, entropy_beta, sess, global_network)
+        super(ActorCritic, self).__init__(
+            in_size, action_size, scope,
+            lr_actor, lr_critic,
+            entropy_beta, sess, global_network)
 
     def _build_network(self, input_hold):
         actor_name = self.scope + '/actor'
         critic_name = self.scope + '/critic'
         with tf.variable_scope('actor'):
             net = Deep_layer.conv2d_pool(input_layer=input_hold,
-                                         channels=[32,64,64],
-                                         kernels=[5,3,2],
-                                         pools=[2,2,1],
-                                         strides=[2,2,1],
+                                         channels=[32, 64, 64],
+                                         kernels=[5, 3, 2],
+                                         pools=[2, 2, 1],
+                                         strides=[2, 2, 1],
                                          flatten=True)
             net = layers.fully_connected(net, 128)
             actor = layers.fully_connected(net,
@@ -221,4 +227,3 @@ class ActorCritic(a3c):
         c_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=critic_name)
 
         return actor, critic, a_vars, c_vars
-
